53115e9c888b7099b03646a9568949a6dc687b7a
andi25 <adallapozza@hotmail.com>
2021-08-24 22:44:50 -0300

    test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Andi es el mejor</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Andi es el mejor</h1>
       </header>
