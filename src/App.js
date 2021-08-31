bb22ac6279d65be84dadf04e9119eaefb0135a0a
andi25 <adallapozza@hotmail.com>
2021-08-31 18:30:43 -0300

    add new navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import './App.css'
-import NavBar from './components/NavBar'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
+
+
+
 
 
 function App() {
   return (
-    <div className="App bg-dark">
+    <div>
       <NavBar/>
     </div>
   )
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,50 @@
+import logo from '../img/logo.jpg';
+import './NavBar.css';
+import  { FaShoppingCart } from "react-icons/fa";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+
+
+function NavBar() {
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <img src= { logo } alt="logo" className="img_logo img-thumbnail rounded-circle m-2" style={{ width: '80px' }} />
+                    <Navbar.Brand href="#home">Mercado Loco</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="mr-2 me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                        </Form>
+                        <FaShoppingCart size="2em" color="cornflowerblue" className="m-3" />
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavBar
+
+
+
+
+
+
+
